a02acd8b983485304d00084840e3e104308a3db9
vishnu563 <vishnuprasanth050603@gmail.com>
2024-10-18 17:21:30 +0530

    product page and service page designed

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ document.querySelector('form').addEventListener('submit', function(event) {
     event.preventDefault();
     alert('Thank you for your message! We will get back to you shortly.');
 });
+document.addEventListener("DOMContentLoaded", function() {
+    const currentYear = new Date().getFullYear();
+    document.getElementById("currentYear").textContent = currentYear;
+});
