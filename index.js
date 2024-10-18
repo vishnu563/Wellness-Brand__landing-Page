58e33e021676cb82d4b1586ac1042983165a7c1a
vishnu563 <vishnuprasanth050603@gmail.com>
2024-10-18 11:02:17 +0530

    Initialized

---
+++ index.js
@@ -0,0 +1,4 @@
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Thank you for your message! We will get back to you shortly.');
+});
